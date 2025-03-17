55230ae2b1fb486870379475086ad901b58b79e7
Ram Tobolski <ramtob@gmail.com>
2025-03-17 16:41:00 +0200

    Adding tests

--- magic_store.spec.py
+++ magic_store.spec.py
@@ -1,2 +1,7 @@
+import unittest
 import magic_store as ms
 
+class TestMagicStore(unittest.TestCase):
+    def test_cart_length(self):
+        ms.cart = ["Magic Wand", "Flying Broom", "Invisibility Cloak"]
+        assert len(ms.cart) == 3, "AssertionError: The cart should contain exactly 3 magic items!"
