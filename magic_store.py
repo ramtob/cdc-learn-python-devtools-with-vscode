6c32b49217f3d7af48103940682bf729f669cabd
Ram Tobolski <ramtob@gmail.com>
2025-03-17 16:00:08 +0200

    Adding file magic_store.py to practice unit testing

---
+++ magic_store.py
@@ -0,0 +1,21 @@
+def add_to_cart(item, cart):
+    # Adds an item to the cart if it's not already there.
+    if item in cart:
+        return f"{item} is already in the cart!"
+    cart.append(item)
+    return f"{item} has been added to the cart."
+
+def checkout(cart):
+    # Checks out the cart if it contains items.
+    if len(cart) > 0:
+        return f"Checking out with {len(cart)} items!"
+    return "Your cart is empty! Add some magic items first!"
+
+# Calling the functions
+cart = []
+
+# Adding items to the cart
+print(add_to_cart("Magic Wand", cart))  
+print(add_to_cart("Flying Broom", cart)) 
+print(add_to_cart("Invisibility Cloak", cart)) 
+print(add_to_cart("Magic Wand", cart))  
